5d745fbe7a98c5ec6cbb0bc54608760f0fe13ccf
Yigit Yalcin <yigityalciin@hotmail.com>
2023-10-08 00:51:05 +0300

    PW

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,14 +4,14 @@ import type { Metadata } from 'next'
 import { Inter, Space_Grotesk } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
-const spaceGrotesk = Space_Grotesk({
-subsets: ['latin'], 
-weight:['300','400','500','600','700'] 
-})
+const spaceGrotesk = Space_Grotesk({ 
+  subsets: ['latin'], 
+  weight: ['300', '400', '500', '600', '700']
+ })
 
 export const metadata: Metadata = {
   title: 'Pricewise',
-  description: 'Track prices and save money on your online shopping',
+  description: 'Track product prices effortlessly and save money on your online shopping.',
 }
 
 export default function RootLayout({
@@ -22,11 +22,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className='max-w-10xl mx-auto'>
-          <Navbar/>
+        <main className="max-w-10xl mx-auto">
+          <Navbar />
           {children}
         </main>
-        </body>
+      </body>
     </html>
   )
 }
